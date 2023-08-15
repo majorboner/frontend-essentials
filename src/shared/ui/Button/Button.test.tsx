e106dcfa08b846fbbfdb2b56804d5c496f322c72
Major Boner <khodukin88@gmail.com>
2023-08-15 15:40:41 +0700

    tests fix

--- src/shared/ui/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
-
-describe('To be in the doc', () => {
-  test('Test', () => {
-    render(<Button>TEST</Button>);
-    expect(screen.getByText('TEST')).toBeInTheDocument();
-  });
-});
