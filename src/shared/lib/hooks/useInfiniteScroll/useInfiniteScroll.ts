2eceb47a71f20a8fe4efab71373e05e7060e5d15
Major Boner <khodukin88@gmail.com>
2023-10-20 17:51:52 +0700

    unfinite scroll hook hotfix

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -8,10 +8,12 @@ export interface UseInfiniteScrollOptions {
 
 export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfiniteScrollOptions) {
   useEffect(() => {
+    const wrapperElement = wrapperRef.current;
+    const triggerElement = triggerRef.current;
     let observer: IntersectionObserver | null = null;
     if (callback) {
       const options = {
-        root: wrapperRef.current,
+        root: wrapperElement,
         rootMargin: '0px',
         threshold: 1.0,
       };
@@ -22,13 +24,13 @@ export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfin
         }
       }, options);
 
-      observer.observe(triggerRef.current);
+      observer.observe(triggerElement);
     }
 
     return () => {
-      if (observer) {
+      if (observer && triggerElement) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        observer.unobserve(triggerRef.current);
+        observer.unobserve(triggerElement);
       }
     };
   }, [callback, triggerRef, wrapperRef]);
