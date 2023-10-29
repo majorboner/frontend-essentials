b41fa0fba919e4cad19e56bc19acb5803566e7dc
Major Boner <khodukin88@gmail.com>
2023-10-29 19:25:06 +0700

    add: lazy lib loading, gestures, spring

--- src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -7,6 +7,7 @@ import { Popover } from 'shared/ui/Popups';
 import NotificationIcon from 'shared/assets/icons/notification.svg';
 import { BrowserView, MobileView } from 'react-device-detect';
 import { Drawer } from 'shared/ui/Drawer/Drawer';
+import { AnimationProvider } from 'shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -43,9 +44,11 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
       </BrowserView>
       <MobileView>
         {trigger}
-        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-          <NotificationList />
-        </Drawer>
+        <AnimationProvider>
+          <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+            <NotificationList />
+          </Drawer>
+        </AnimationProvider>
       </MobileView>
     </div>
 
