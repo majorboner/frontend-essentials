b6504f5bfd01e886b3c308cf233ec6c8e526acf2
Major Boner <khodukin88@gmail.com>
2023-11-01 16:14:23 +0700

    fix: type export

--- src/shared/ui/Popups/index.ts
+++ src/shared/ui/Popups/index.ts
@@ -2,4 +2,6 @@ export { Dropdown } from './ui/Dropdown/Dropdown';
 
 export { ListBox } from './ui/ListBox/ListBox';
 
+export type { ListBoxOptions } from './ui/ListBox/ListBox';
+
 export { Popover } from './ui/Popover/Popover';
