0e9c51056f4f77cc6fc5119edc625a679b362f5d
Major Boner <khodukin88@gmail.com>
2023-11-01 20:13:28 +0700

    fix: jest aliases

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -34,6 +34,7 @@ const config: Config = {
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   reporters: [
     'default',
